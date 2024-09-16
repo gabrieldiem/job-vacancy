7314c94a64f65b1a8d058a0496770dea58ee42bc
Nicolas <nicopaez@computer.org>
2024-09-16 13:58:18 +0000

    Update reports.rb

--- app/controllers/reports.rb
+++ app/controllers/reports.rb
@@ -3,7 +3,6 @@ JobVacancy::App.controllers :reports, provides: [:json] do
     repo = JobOfferRepository.new
     offer_counter = OfferCounter.new(repo)
     report = {
-      month: '2022-09',
       total_active_offers: offer_counter.count_active
     }
     return report.to_json
