2020ea3e4df32f471c8d46e682e16f59f914d039
gabrieldiem <gabriel.diem5@gmail.com>
2025-03-30 11:24:48 -0300

    Added regression tests for already present fields in JobOffer

--- spec/models/job_offer_spec.rb
+++ spec/models/job_offer_spec.rb
@@ -14,6 +14,16 @@ describe JobOffer do
     end
   end
 
+  it 'should have a description of "Remote" when created with it' do
+    job_offer = described_class.new(title: 'a title', description: 'Remote')
+    expect(job_offer.description).to eq 'Remote'
+  end
+
+  it 'should have a location of "Korea" when created with it' do
+    job_offer = described_class.new(title: 'a title', location: 'Korea')
+    expect(job_offer.location).to eq 'Korea'
+  end
+
   it 'should have a salary of 10000 when created with it' do
     job_offer = described_class.new(title: 'a title', salary: 10_000)
     expect(job_offer.salary).to eq 10_000
@@ -22,5 +32,12 @@ describe JobOffer do
   it 'should have a salary of 200 when created with it' do
     job_offer = described_class.new(title: 'a title', salary: 200)
     expect(job_offer.salary).to eq 200
+  end
+
+  it 'should have a salary of 300, description of "Remote" and location of "Korea" when created with it' do
+    job_offer = described_class.new(title: 'a title', salary: 300, description: 'Remote', location: 'Korea')
+    expect(job_offer.salary).to eq 300
+    expect(job_offer.description).to eq 'Remote'
+    expect(job_offer.location).to eq 'Korea'
   end
 end
