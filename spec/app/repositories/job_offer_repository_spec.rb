d554903c8982830491cac1df500e5552ff90e879
gabrieldiem <gabriel.diem5@gmail.com>
2025-03-30 17:46:45 -0300

    Refactored tests of JobOfferRepository to reduce code duplication

--- spec/app/repositories/job_offer_repository_spec.rb
+++ spec/app/repositories/job_offer_repository_spec.rb
@@ -13,57 +13,56 @@ describe JobOfferRepository do
     Date.today
   end
 
+  def create_job_offer(title, salary, other_data = { is_active: true })
+    JobOffer.new(title:, updated_on: today, user_id: owner.id, salary:, **other_data)
+  end
+
   def save_and_retrieve_by_id(job_offer)
     repository.save(job_offer)
     repository.find(job_offer.id)
   end
 
   it 'A JobOffer with title "Dev", description "Remote" is saved and retrieved correctly' do
-    job_offer = JobOffer.new(title: 'Dev', description: 'Remote', updated_on: today, is_active: true,
-                             user_id: owner.id, salary: 0)
+    job_offer = create_job_offer('Dev', 0, { description: 'Remote' })
     found_offer = save_and_retrieve_by_id(job_offer)
     expect(found_offer.title).to eq 'Dev'
     expect(found_offer.description).to eq 'Remote'
   end
 
   it 'A JobOffer with title "Dev" and location "USA" is saved and retrieved correctly' do
-    job_offer = JobOffer.new(title: 'Dev', location: 'USA', updated_on: today, is_active: true, user_id: owner.id,
-                             salary: 0)
+    job_offer = create_job_offer('Dev', 0, { location: 'USA' })
     found_offer = save_and_retrieve_by_id(job_offer)
     expect(found_offer.title).to eq 'Dev'
     expect(found_offer.location).to eq 'USA'
   end
 
   it 'A JobOffer with title "Dev" and is not active is saved and retrieved correctly' do
-    job_offer = JobOffer.new(title: 'Dev', updated_on: today, is_active: false, user_id: owner.id, salary: 0)
+    job_offer = create_job_offer('Dev', 0, { is_active: false })
     found_offer = save_and_retrieve_by_id(job_offer)
     expect(found_offer.title).to eq 'Dev'
     expect(found_offer.is_active).to be false
   end
 
   it 'A JobOffer saved and retreived should have a salary of 10000 when created with it' do
-    job_offer = JobOffer.new(title: 'title', updated_on: today, salary: 10_000, is_active: true, user_id: owner.id)
+    job_offer = create_job_offer('title', 10_000)
+
     found_offer = save_and_retrieve_by_id(job_offer)
     expect(found_offer.salary).to eq 10_000
   end
 
   describe 'deactive_old_offers' do
+    def create_job_offer_with_update_date(title, salary, update_on)
+      JobOffer.new(title:, updated_on: update_on, user_id: owner.id, salary:, is_active: true)
+    end
+
     let!(:today_offer) do
-      today_offer = JobOffer.new(title: 'a title',
-                                 updated_on: Date.today,
-                                 is_active: true,
-                                 user_id: owner.id,
-                                 salary: 0)
+      today_offer = create_job_offer_with_update_date('a title', 0, Date.today)
       repository.save(today_offer)
       today_offer
     end
 
     let!(:thirty_day_offer) do
-      thirty_day_offer = JobOffer.new(title: 'a title',
-                                      updated_on: Date.today - 45,
-                                      is_active: true,
-                                      user_id: owner.id,
-                                      salary: 0)
+      thirty_day_offer = create_job_offer_with_update_date('a title', 0, Date.today - 45)
       repository.save(thirty_day_offer)
       thirty_day_offer
     end
