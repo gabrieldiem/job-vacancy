4614933fa820fad972bbff537b4eaf37a5b50529
saintttt <alivetheoldschool@hotmail.com>
2025-04-11 16:46:53 -0300

    Added unit test to check that the non-profit organization is well created by the subscription factory

--- spec/models/subscription_factory_spec.rb
+++ spec/models/subscription_factory_spec.rb
@@ -10,4 +10,10 @@ describe SubscriptionFactory do
     subscription = factory.create_subscription(on_demand_type, an_email)
     expect(subscription.is_a?(OnDemandSubscription)).to be true
   end
+
+  it 'Creates an NonProfitOrganization for its type' do
+    factory = described_class.new
+    subscription = factory.create_subscription(non_profit_org_type, an_email)
+    expect(subscription.is_a?(NonProfitOrganizationSubscription)).to be true
+  end
 end
