961d25d9800ccd00890e4914e6f24ec6f4ea6250
saintttt <alivetheoldschool@hotmail.com>
2025-04-11 00:20:25 -0300

    Added unit test to check the amount of more than 1 offer for an On Demand subscription.

--- spec/models/on_demand_subscription_spec.rb
+++ spec/models/on_demand_subscription_spec.rb
@@ -5,4 +5,15 @@ describe OnDemandSubscription do
     subscription = described_class.new
     expect(subscription.calculate_cost([JobOffer.new(title: 'a title', salary: 0)])).to eq 10
   end
+
+  it 'Cost of 2 offers is 20' do
+    offers = []
+
+    2.times do
+      offers.push JobOffer.new(title: 'a title', salary: 0)
+    end
+
+    subscription = described_class.new
+    expect(subscription.calculate_cost(offers)).to eq 20
+  end
 end
