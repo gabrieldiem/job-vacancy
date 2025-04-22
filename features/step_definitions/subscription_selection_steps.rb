ff7c2ac209f9f2c4fdc6f8c3647c10534dfc98cc
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-22 16:38:02 -0300

    acceptance-test-2: added gherkins for subscription selection

---
+++ features/step_definitions/subscription_selection_steps.rb
@@ -0,0 +1,61 @@
+Given('I press the button to see the subscription types') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be able to see all of the subscription types available') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I fill the registration form and I select the subscription type “on-demand”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I press the Register button') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see the subscription type under my email') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I fill the registration form and don’t select a subscription') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see the error message “can’t be blank”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I fill the registration form and I select the subscription type “non-profit organization”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('email is “example@place.org”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('email is “example@place.com”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see the error message “must have .org mail for non commercial organization subscription”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I have a “non-profit organization” subscription with email “example@place.org”') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('add {int} active job offers') do |_int|
+  # Given('add {float} active job offers') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I press the button to activate the 8th active job offer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see the error message “limit exceeded for non commercial organization subscription. Max is {int} active offers”') do |_int|
+  # Then('I should see the error message “limit exceeded for non commercial organization subscription. Max is {float} active offers”') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/subscription_selection.feature
@@ -0,0 +1,36 @@
+Feature: Subscription selection in registration
+  As a JobOfferer
+  I want to select my subscription type
+  So I can register to publish offers
+
+  Scenario: US-2.1 Show all subscription types
+    Given I press the button to see the subscription types
+    Then I should be able to see all of the subscription types available
+
+  Scenario: US-2.2 Successful registration with subscription On Demand
+    Given I fill the registration form and I select the subscription type “on-demand”
+    When I press the Register button
+    Then I should see the subscription type under my email
+
+  Scenario: US-2.3 Cannot register without subscription
+    Given I fill the registration form and don’t select a subscription
+    When I press the Register button
+    Then I should see the error message “can’t be blank”
+
+  Scenario: US-2.4 Successful registration of NonCommercialOrganization subscription type
+    Given I fill the registration form and I select the subscription type “non-profit organization”
+    And email is “example@place.org”
+    When I press the Register button
+    Then I should see the subscription type under my email
+
+  Scenario: US-2.5 Cannot register with non-org email and NonCommercialOrganization subscription type
+    Given I fill the registration form and I select the subscription type “non-profit organization”
+    And email is “example@place.com”
+    When I press the Register button
+    Then I should see the error message “must have .org mail for non commercial organization subscription”
+
+  Scenario: US-2.6 Cannot active 8th job offer with non-profit organization
+    Given I have a “non-profit organization” subscription with email “example@place.org”
+    And add 7 active job offers
+    When I press the button to activate the 8th active job offer
+    Then I should see the error message “limit exceeded for non commercial organization subscription. Max is 7 active offers”
