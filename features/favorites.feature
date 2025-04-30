65cc28bac4999728c4fe601856eecee9acb4b613
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-30 11:39:56 -0300

    acceptance-test-6: added gherkins

---
+++ features/favorites.feature
@@ -0,0 +1,40 @@
+Feature: Mark job offers as favourites
+
+  Background:
+    Given a job offer exists created by a job offerer
+    And I am logged in as a registered user
+    And I have active job offers
+
+  Scenario: US-6.1 Mark a job offer as favorite
+    When I visit the job offers page
+    And I click the button to mark as favourite for the job offer "Software Engineer"
+    Then I see the message "Job offer marked as favourite"
+
+  Scenario: US-6.2 Unmark a job offer as favorite
+    Given I marked the job offer "Senior Engineer" as favorite
+    When I visit the job offers page
+    And I click the button to mark as favourite
+    Then I see the message "Job offer unmarked as favorite"
+
+  Scenario: US-6.3 Cannot mark my own job offer as favorite
+    Given I am logged in as a job offerer
+    When I visit the job offers page
+    And I click the button to mark as favourite on my own job offer
+    Then I should not see the message "Job offer marked as favorite"
+
+  Scenario: US-6.4 Cannot see the button to mark as favourite if not logged in
+    Given I am not logged in
+    When I visit the job offers page
+    Then I do not see the button to mark as favourite for the offers
+
+  Scenario: US-6.5 Unmarked as favourite all job offers
+    Given I have marked one job offer as favorite
+    When I visit the job offers page
+    And I click the "Unfavorite All" button
+    Then I see the message "All favorites removed"
+
+  Scenario: US-6.6 View the favorites count on my job offers
+    Given I am logged in a job offerer
+    And 3 users have marked my job offer as favorite
+    When I visit my job offers page
+    Then I see "3 favorites" for that job offer
---
+++ features/favorites_steps.rb
@@ -0,0 +1,76 @@
+Given('a job offer exists created by a job offerer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am logged in as a registered user') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I have active job offers') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I visit the job offers page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click the button to mark as favourite for the job offer {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see the message {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I marked the job offer {string} as favorite') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click the button to mark as favourite') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am logged in as a job offerer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click the button to mark as favourite on my own job offer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should not see the message {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am not logged in') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I do not see the button to mark as favourite for the offers') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I have marked one job offer as favorite') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click the {string} button') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am logged in a job offerer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('{int} users have marked my job offer as favorite') do |_int|
+  # Given('{float} users have marked my job offer as favorite') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I visit my job offers page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see {string} for that job offer') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
