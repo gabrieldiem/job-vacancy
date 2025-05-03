21c50ef35fa58246a0f298734bb8c64a6440343f
saintttt <alivetheoldschool@hotmail.com>
2025-05-02 23:52:35 -0300

    acceptance-test-3: gherkins and steps defined

---
+++ features/step_definitions/job_offer_experience_steps.rb
@@ -0,0 +1,36 @@
+When('I create a job offer with {string} years of required experience') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see {string} required experience in the job offers list') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see it in my offers as well') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see {string} in the job offers list') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I create a job offer without filling the required experience') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see the error {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('an existing job offer with {int} years of experience required') do |_int|
+  # Given('an existing job offer with {float} years of experience required') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I edit the job offer and change the experience to {string} years') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the job offer should now show {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
